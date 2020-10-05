23f2f987e1e5d823b64c3adf49ca59470448deee
Melvin Tang <mxcodeyard@gmail.com>
2020-10-06 06:49:22 +0800

    Centered pages content

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Container from 'react-bootstrap/container';
 import Navigation from './Navigation';
 import Main from './pages/Main';
 import Latest from './pages/Latest';
@@ -7,16 +8,19 @@ import Products from './pages/Products';
 import Cart from './pages/Cart';
 import Login from './pages/Login';
 
+
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Navigation />
-        <Route path='/' exact component={Main} />
-        <Route path='/latest' exact component={Latest} />
-        <Route path='/products' component={Products} />
-        <Route path='/cart' component={Cart} />
-        <Route path='/login' component={Login} />
+        <Container>
+          <Route path='/' exact component={Main} />
+          <Route path='/latest' exact component={Latest} />
+          <Route path='/products' component={Products} />
+          <Route path='/cart' component={Cart} />
+          <Route path='/login' component={Login} />
+        </Container>
       </BrowserRouter>
     </div>
   );
