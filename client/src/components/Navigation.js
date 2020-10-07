5c7e40b6321948d85981a77e29605796ad4274dd
Melvin Tang <mxcodeyard@gmail.com>
2020-10-08 06:52:03 +0800

    Update navbar

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Navigation = () => {
@@ -7,16 +7,41 @@ const Navigation = () => {
     <div>
       <Navbar bg='light' expand='lg'>
         <Container>
-          <Navbar.Brand as={Link} to='/'>electron</Navbar.Brand>
+          <Navbar.Brand as={Link} to='/'>
+            electron
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='mr-auto'>
-              <Nav.Link as={Link} to='/latest'>Latest</Nav.Link>
-              <Nav.Link as={Link} to='/products'>Products</Nav.Link>
+              <Nav.Link as={Link} to='/latest'>
+                Latest
+              </Nav.Link>
+              <NavDropdown title='Products' id='basic-nav-dropdown'>
+                <NavDropdown.Item as={Link} to='/products'>
+                  All Products
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item as={Link} to='/consoles'>
+                  Consoles
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to='/laptops'>
+                  Laptops
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to='/graphic-cards'>
+                  Graphic Cards
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to='/accessories'>
+                  Accessories
+                </NavDropdown.Item>
+              </NavDropdown>
             </Nav>
             <Nav className='ml-auto'>
-              <Nav.Link as={Link} to='/cart'>Cart</Nav.Link>
-              <Nav.Link as={Link} to='/login'>Sign In</Nav.Link>
+              <Nav.Link as={Link} to='/cart'>
+                Cart
+              </Nav.Link>
+              <Nav.Link as={Link} to='/login'>
+                Sign In
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
