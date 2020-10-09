f03cbac1d33e892506388b07e0231e0a749be56c
Melvin Tang <mxcodeyard@gmail.com>
2020-10-09 16:48:21 +0800

    Update product category pages

--- client/src/pages/laptops/Laptops.js
+++ client/src/pages/laptops/Laptops.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Spinner from '../../components/loader/Loader';
 import ProductItem from '../../components/product-item/ProductItem';
-import products from '../../data/products';
+import { productsRequest } from '../../actions/productsActions';
 
 const Laptops = () => {
+  const dispatch = useDispatch();
+  const products = useSelector(({ products }) => products.products);
+  const loading = useSelector(({ products }) => products.loading);
+
+  useEffect(() => {
+    dispatch(productsRequest());
+  }, [dispatch]);
   return (
     <div>
       <h2 className='my-4'>Laptops</h2>
-      {products
-        .filter((product) => product.category === 'laptops')
-        .map((product) => (
-          <ProductItem key={product.id} product={product} />
-        ))}
+      {loading ? (
+        <Spinner loaderMsg='Loading...' />
+      ) : (
+        products
+          .filter((product) => product.category === 'laptops')
+          .map((product) => <ProductItem key={product._id} product={product} />)
+      )}
     </div>
   );
 };
