5ccfe8603a73b60e87bbae849affcd285f2782b1
Melvin Tang <mxcodeyard@gmail.com>
2020-10-09 15:55:17 +0800

    Create & add spinner component to products page

---
+++ client/src/components/loader/Loader.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Spinner from 'react-bootstrap/spinner';
+
+const Loader = ({ loaderMsg }) => {
+  return (
+    <div className='text-center'>
+      <Spinner
+        animation='border'
+        style={{ width: '3rem', height: '3rem' }}
+        variant='primary'
+      />
+      <span>{loaderMsg}</span>
+    </div>
+  );
+};
+
+export default Loader;
--- client/src/pages/products/Products.js
+++ client/src/pages/products/Products.js
@@ -2,10 +2,12 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import ProductItem from '../../components/product-item/ProductItem';
 import { productsRequest } from '../../actions/productsActions';
+import Spinner from '../../components/loader/Loader';
 
 const Products = () => {
   const dispatch = useDispatch();
   const products = useSelector(({ products }) => products.products);
+  const loading = useSelector(({ products }) => products.loading);
 
   useEffect(() => {
     dispatch(productsRequest());
@@ -14,9 +16,13 @@ const Products = () => {
   return (
     <div>
       <h2 className='my-4'>All Products</h2>
-      {products.map((product) => (
-        <ProductItem key={product._id} product={product} />
-      ))}
+      {loading ? (
+        <Spinner loaderMsg='Loading...' />
+      ) : (
+        products.map((product) => (
+          <ProductItem key={product._id} product={product} />
+        ))
+      )}
     </div>
   );
 };
