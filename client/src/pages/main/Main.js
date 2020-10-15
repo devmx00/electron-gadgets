18d3df950a079de1a4ef823fe97748a89dfbf3c6
Melvin Tang <mxcodeyard@gmail.com>
2020-10-15 18:56:42 +0800

    Update styles

--- client/src/pages/main/Main.js
+++ client/src/pages/main/Main.js
@@ -15,22 +15,76 @@ const Main = () => {
         <h1>Electron Gadgets</h1>
         <p>
           The place where you'll find the latest electronic products at
-          extravagant prices! Your welcome.
+          competitive prices!
         </p>
         <Button variant='primary' as={Link} to='/products'>
-          See More
+          Shop Now
         </Button>
       </Jumbotron>
+      <h3>Shop Categories</h3>
+      <hr />
       <Row>
-        <Col sm={6} md={3} className='my-4'>
+        {/* <Col sm={6} md={3} className='my-4'>
           <Card className='p-2'>
             <Image src='/img/ps-5.png' className='img-fluid img-size' />
             <Button as={Link} to='/products/category/1'>
               Consoles
             </Button>
           </Card>
+        </Col> */}
+
+        <Col sm={6} lg={3}>
+          <Link to='/products/category/1'>
+            <Card className='bg-dark text-white m-2 p-2'>
+              <Image
+                src='/img/ps-5.png'
+                alt='Card image'
+                className='img-fluid img-size'
+              />
+              <Card.Title className='text-center mt-2'>Consoles</Card.Title>
+            </Card>
+          </Link>
         </Col>
-        <Col sm={6} md={3} className='my-4'>
+        <Col sm={6} lg={3}>
+          <Link to='/products/category/2'>
+            <Card className='bg-dark text-white m-2 p-2'>
+              <Card.Img
+                src='/img/xps-13.png'
+                alt='Card image'
+                className='img-fluid img-size'
+              />
+              <Card.Title className='text-center mt-2'>Laptops</Card.Title>
+            </Card>
+          </Link>
+        </Col>
+        <Col sm={6} lg={3}>
+          <Link to='/products/category/3'>
+            <Card className='bg-dark text-white m-2 p-2'>
+              <Card.Img
+                src='/img/nvidia-rtx-3090.png'
+                alt='Card image'
+                className='img-fluid img-size'
+              />
+              <Card.Title className='text-center mt-2'>
+                Graphic Cards
+              </Card.Title>
+            </Card>
+          </Link>
+        </Col>
+        <Col sm={6} lg={3}>
+          <Link to='/products/category/4'>
+            <Card className='bg-dark text-white m-2 p-2'>
+              <Card.Img
+                src='/img/razer-13.png'
+                alt='Card image'
+                className='img-fluid img-size'
+              />
+              <Card.Title className='text-center mt-2'>Consoles</Card.Title>
+            </Card>
+          </Link>
+        </Col>
+
+        {/* <Col sm={6} md={3} className='my-4'>
           <Card className='p-2'>
             <Image src='/img/xps-13.png' className='img-fluid img-size' />
             <Button as={Link} to='/products/category/2'>
@@ -56,7 +110,7 @@ const Main = () => {
               Accessories
             </Button>
           </Card>
-        </Col>
+        </Col> */}
       </Row>
     </div>
   );
